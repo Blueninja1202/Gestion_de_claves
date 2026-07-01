88d7f309d9b99ae7e5f75c4ac8da4d848632ce28
Luis Hernandez <129894124+Blueninja1202@users.noreply.github.com>
2026-07-01 17:07:47 -0400

    Add files via upload

---
+++ build/app/intermediates/cxx/debug/53fi4m5d/logs/armeabi-v7a/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"C:\\Users\\lhernandez\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\lhernandez\\Desktop\\Proyect\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\scripts" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=24" ^
+  "-DANDROID_PLATFORM=android-24" ^
+  "-DANDROID_ABI=armeabi-v7a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a" ^
+  "-DANDROID_NDK=C:\\Users\\lhernandez\\AppData\\Local\\Android\\sdk\\ndk\\28.2.13676358" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\lhernandez\\AppData\\Local\\Android\\sdk\\ndk\\28.2.13676358" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\lhernandez\\AppData\\Local\\Android\\sdk\\ndk\\28.2.13676358\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\lhernandez\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\lhernandez\\Desktop\\gestor_claves\\gestor\\build\\app\\intermediates\\cxx\\debug\\53fi4m5d\\obj\\armeabi-v7a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\lhernandez\\Desktop\\gestor_claves\\gestor\\build\\app\\intermediates\\cxx\\debug\\53fi4m5d\\obj\\armeabi-v7a" ^
+  "-BC:\\Users\\lhernandez\\Desktop\\gestor_claves\\gestor\\build\\.cxx\\debug\\53fi4m5d\\armeabi-v7a" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli ^
+  "-DCMAKE_BUILD_TYPE=debug"
